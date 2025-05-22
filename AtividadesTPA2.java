7382cd4a160ea873dbaafe069daae65a75a3389c
Pedro Rodrigues Pimentel <pimentelpedro258@gmail.com>
2025-05-21 22:39:23 -0300

    continuação da tarefa de TPA (scanner

---
+++ AtividadesTPA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividadestpa2;
+
+/**
+ *
+ * @author pedro
+ */
+public class AtividadesTPA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
